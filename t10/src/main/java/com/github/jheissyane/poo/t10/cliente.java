913a341de8da10e793ba276600c3ac4428400102
jheissyane souza <jheissyane@discente.ufg.br>
2023-07-03 12:02:57 -0300

    att

--- t10/src/main/java/com/github/jheissyane/poo/t10/cliente.java
+++ t10/src/main/java/com/github/jheissyane/poo/t10/cliente.java
@@ -1,37 +1,36 @@
 package com.github.jheissyane.main.t10;
 
 class Cliente {
-    private String nome;
-    private Pedido pedido;
+    private String pessoa;
+    private Demanda requisito;
     private Bicicleta bicicleta;
 
     public void cadastrarCliente() {
-        Cliente cliente = new Cliente();
-        this.cadastrarCliente("João", "Bicicleta com problema no freio", "Caloi", "Azul", 500);
+        cadastrarCliente("Felipe", "Bicicleta com pneu furado", "Specialized", "Vermelha", 500);
     }
 
-    public void cadastrarCliente(String nome, String descricao, String modelo, String cor, float preco) {
-        this.nome = nome;
-        this.pedido = new Pedido();
-        this.pedido.definirDescricao(descricao);
+    public void cadastrarCliente(String pessoa, String descricao, String marca, String cor, float preco) {
+        this.pessoa = pessoa;
+        this.requisito = new Demanda();
+        this.requisito.definirDescricao(descricao);
         this.bicicleta = new Bicicleta();
-        this.bicicleta.definirDescricao(modelo, cor, preco);
+        this.bicicleta.definirDescricao(marca, cor, preco);
     }
 }
 
 class Bicicleta {
-    private String modelo;
+    private String marca;
     private String cor;
     private float preco;
 
-    public void definirDescricao(String modelo, String cor, float preco) {
-        this.modelo = modelo;
+    public void definirDescricao(String marca, String cor, float preco) {
+        this.marca = marca;
         this.cor = cor;
         this.preco = preco;
     }
 }
 
-class Pedido {
+class Demanda {
     private String descricao;
 
     public void definirDescricao(String descricao) {
